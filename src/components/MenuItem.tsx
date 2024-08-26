c686601c3299b47d71e6b15994fa62840239ef4c
kulde <ku1de@icloud.com>
2024-08-26 23:50:21 +0200

    style(order): margin left to separate from menus column

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -8,7 +8,7 @@ type MenuItemProps = {
 export default function MenuItem ({item, addItem} : MenuItemProps) {
     return (
         <button
-            className="border-2 border-slate-950 hover:bg-slate-200 w-full p-2 flex justify-between mb-2" onClick={() => addItem(item)}>
+            className="border-2 border-slate-950 hover:bg-slate-200 w-full p-2 flex justify-between rounded-lg" onClick={() => addItem(item)}>
             <p>{item.name}</p>
             <p className="font-black">€{item.price}</p>
         </button>
