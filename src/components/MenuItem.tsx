7c28d494330dbc431376031daa76ba290c3202d8
kulde <ku1de@icloud.com>
2024-08-26 22:53:55 +0200

    feat(menu): buttons for each item added

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -6,9 +6,10 @@ type MenuItemProps = {
 
 export default function MenuItem ({item} : MenuItemProps) {
     return (
-    <>
+    <button
+    className="border-2 border-slate-950 hover:bg-slate-200 w-full p-2 flex justify-between mb-2">
         <p>{item.name}</p>
         <p className="font-black">€{item.price}</p>
-    </>
+    </button>
     )
 }
