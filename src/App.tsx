09aa412fc4135a0ff813a565e01b3a1696ad527f
kulde <ku1de@icloud.com>
2024-08-26 21:46:32 +0200

    chore(database): load initial menu data

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ function App() {
 
   return (
     <>
-    <header className="bg-slate-400 py-5">
-      <h1 className="text-center text-3xl font-black">Tip calculator</h1>
-    </header>
+      <header className="bg-slate-400 py-5">
+        <h1 className="text-center text-3xl font-black">Tip calculator</h1>
+      </header>
+
+
     </>
   )
 }
---
+++ src/data/db.ts
@@ -0,0 +1,127 @@
+export const menuItems = [
+    {
+      "id": 1,
+      "name": "Bruschetta al Pomodoro",
+      "price": 5
+    },
+    {
+      "id": 2,
+      "name": "Carpaccio",
+      "price": 6
+    },
+    {
+      "id": 3,
+      "name": "Arancini",
+      "price": 8
+    },
+    {
+      "id": 4,
+      "name": "Prosciutto e Melone",
+      "price": 7
+    },
+    {
+      "id": 5,
+      "name": "Caprese",
+      "price": 7
+    },
+    {
+      "id": 6,
+      "name": "Lasagna",
+      "price": 18
+    },
+    {
+      "id": 7,
+      "name": "Ravioli",
+      "price": 20
+    },
+    {
+      "id": 8,
+      "name": "Spaghetti Carbonara",
+      "price": 15
+    },
+    {
+      "id": 9,
+      "name": "Risotto ai Funghi",
+      "price": 15
+    },
+    {
+      "id": 10,
+      "name": "Osso Buco",
+      "price": 20
+    },
+    {
+      "id": 11,
+      "name": "Pollo alla Parmigiana",
+      "price": 20
+    },
+    {
+      "id": 12,
+      "name": "Tagliatelle al Ragù",
+      "price": 25
+    },
+    {
+      "id": 13,
+      "name": "Melanzane alla Parmigiana",
+      "price": 7
+    },
+    {
+      "id": 14,
+      "name": "Patate al Rosmarino",
+      "price": 7
+    },
+    {
+      "id": 15,
+      "name": "Tiramisu",
+      "price": 8
+    },
+    {
+      "id": 16,
+      "name": "Panna Cotta",
+      "price": 8
+    },
+    {
+      "id": 17,
+      "name": "Cannoli",
+      "price": 8
+    },
+    {
+      "id": 18,
+      "name": "Gelato",
+      "price": 6
+    },
+    {
+      "id": 19,
+      "name": "Zabaglione",
+      "price": 8
+    },
+    {
+      "id": 20,
+      "name": "Espresso",
+      "price": 3
+    },
+    {
+      "id": 21,
+      "name": "Cappuccino",
+      "price": 3
+    },
+    {
+      "id": 22,
+      "name": "Limoncello",
+      "price": 4
+    },
+    {
+      "id": 23,
+      "name": "Negroni",
+      "price": 3
+    },
+    {
+      "id": 24,
+      "name": "Prosecco",
+      "price": 8
+    },
+    {
+      "id": 25,
+      "name": "Amaro",
+      "price": 10
+    },
+]
