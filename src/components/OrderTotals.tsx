d6ac39c5db4ef04f473743ce21fe050ab6738a78
kulde <ku1de@icloud.com>
2024-08-27 21:45:58 +0200

    feat(count): total, tip, full total text added

---
+++ src/components/OrderTotals.tsx
@@ -0,0 +1,23 @@
+export default function OrderTotals() {
+  return (
+    <>
+    <div className="space-y-3">
+        <h2 className="font-black text-2xl">Totals and tips:</h2>
+            <p>Total: {""}
+                <span className="font-bold">€0</span>
+            </p>
+
+            <p>Tip: {""}
+                <span className="font-bold">€0</span>
+            </p>
+
+            <p>Full total: {""}
+                <span className="font-bold">€0</span>
+            </p>
+    </div>
+
+
+        <button></button>
+    </>
+    )
+}
