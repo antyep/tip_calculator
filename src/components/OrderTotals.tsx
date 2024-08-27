381d7fcdadd7a634cb8660fa1f1534f5973319b4
kulde <ku1de@icloud.com>
2024-08-27 22:59:56 +0200

    feat(count): correctly update total, tip, and subtotal based on tip percentage

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -12,6 +12,8 @@ export default function OrderTotals({order, tip} : OrderTotalsProps) {
     const subtotalAmount = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
 
     const tipAmount = useMemo(() => subtotalAmount * tip, [tip, order])
+    const totalAmount = useMemo(() => subtotalAmount + tipAmount, [tip, order])
+
 
   return (
     <>
@@ -26,7 +28,7 @@ export default function OrderTotals({order, tip} : OrderTotalsProps) {
             </p>
 
             <p>Total: {""}
-                <span className="font-bold">€0</span>
+                <span className="font-bold">{formatCurrency(totalAmount)}</span>
             </p>
     </div>
 
